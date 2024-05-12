397d0aef6f2b16b6593d25e4b40fb234290c2e60
darshansatapara <109288106+darshansatapara@users.noreply.github.com>
2024-05-12 23:23:03 +0530

    github mistake commite after solve

--- backend/api/CustomerApi.js
+++ backend/api/CustomerApi.js
@@ -1,20 +1,15 @@
 // Import required modules
 const express = require("express");
 const router = express.Router();
-<<<<<<< HEAD
-const Customer = require("../models/AddCustomerModel"); 
-=======
-const Customer = require("../models/AddCustomerModel"); // Assuming your model is defined in Customer.js
->>>>>>> ed1c452c31e5955f55201279ac32c3d21566a77d
+
+const Customer = require("../models/AddCustomerModel");
 
 // POST route to add a new customer
 router.post("/addnewCustomer", async (req, res) => {
   try {
-<<<<<<< HEAD
-=======
     console.log("data received");
     // Fetch the last customer from the database to get the last customerID
->>>>>>> ed1c452c31e5955f55201279ac32c3d21566a77d
+
     const lastCustomer = await Customer.findOne()
       .sort({ customerID: -1 })
       .maxTimeMS(10000);
@@ -22,15 +17,10 @@ router.post("/addnewCustomer", async (req, res) => {
     // Increment the last customerID by 1 to get the new customerID
     const newCustomerID = lastCustomer ? lastCustomer.customerID + 1 : 1;
 
-<<<<<<< HEAD
-    const { customerName, mobileNumber, city, address, pincode } = req.body;
-
-=======
-    // data from request body
     const { customerName, mobileNumber, city, address, pincode } = req.body;
 
     // Create a new customer instance with the new customerID
->>>>>>> ed1c452c31e5955f55201279ac32c3d21566a77d
+
     const newCustomer = new Customer({
       customerID: newCustomerID,
       customerName,
@@ -40,24 +30,20 @@ router.post("/addnewCustomer", async (req, res) => {
       pincode,
     });
     console.log(newCustomer);
-<<<<<<< HEAD
 
     await newCustomer.save();
 
-=======
     // Save the new customer to the database
     await newCustomer.save();
 
     // Send success response
->>>>>>> ed1c452c31e5955f55201279ac32c3d21566a77d
+
     res
       .status(201)
       .json({ message: "Customer added successfully.", customer: newCustomer });
   } catch (error) {
-<<<<<<< HEAD
-=======
     // If any error occurs, send error response
->>>>>>> ed1c452c31e5955f55201279ac32c3d21566a77d
+
     console.error(error);
     res.status(500).json({ message: "Internal Server Error" });
   }
@@ -66,21 +52,14 @@ router.post("/addnewCustomer", async (req, res) => {
 //get all customers to show in CustomerDetailPage
 router.get("/allcustomers", async (req, res) => {
   try {
-<<<<<<< HEAD
     const customers = await Customer.find();
     res.json(customers);
-=======
-    // Fetch all customers from the database
-    const customers = await Customer.find();
-    res.json(customers); // Send the fetched customers as JSON response
->>>>>>> ed1c452c31e5955f55201279ac32c3d21566a77d
   } catch (error) {
     console.error("Error fetching customers:", error);
     res.status(500).json({ message: "Internal Server Error" });
   }
 });
 
-<<<<<<< HEAD
 // we can find the customers using mobileNumber or CustomerId
 router.get("/getCustomer/:fatchby", async (req, res) => {
   try {
@@ -107,20 +86,19 @@ router.get("/getCustomer/:fatchby", async (req, res) => {
   }
 });
 
-=======
 router.get("/getCustomer/:customerId", async (req, res) => {
-    try {
-      const customerId = parseInt(req.params.customerId);
-      const customer = await Customer.findOne({ customerID: customerId });
-      if (customer) {
-        res.json(customer);
-      } else {
-        res.status(404).json({ message: "Customer not found" });
-      }
-    } catch (error) {
-      console.error("Error fetching customer data:", error);
-      res.status(500).json({ message: "Internal server error" });
+  try {
+    const customerId = parseInt(req.params.customerId);
+    const customer = await Customer.findOne({ customerID: customerId });
+    if (customer) {
+      res.json(customer);
+    } else {
+      res.status(404).json({ message: "Customer not found" });
     }
-  });
->>>>>>> ed1c452c31e5955f55201279ac32c3d21566a77d
+  } catch (error) {
+    console.error("Error fetching customer data:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
 module.exports = router;
--- backend/models/AddNewOrderModel.js
+++ backend/models/AddNewOrderModel.js
@@ -2,7 +2,6 @@
 const mongoose = require("mongoose");
 
 const OrderSchema = new mongoose.Schema({
-<<<<<<< HEAD
   orderID: { type: Number, required: true },
   mobileNumber: { type: Number, required: true },
   customerID: { type: Number, required: true },
@@ -12,19 +11,7 @@ const OrderSchema = new mongoose.Schema({
   blackPepsiQuantity: { type: Number, default: 0 },
   yellowPepsiQuantity: { type: Number, default: 0 },
   totalAmount: { type: Number, required: true },
-  paymentStatus: { type: String, require: true }, 
-=======
-  mobileNumber: { type: String, required: true },
-  customerId: { type: Number, required: true },
-  customerName: { type: String, required: true },
-  orderDate: { type: Date, required: true },
-  address: { type: String, required: true },
-  pincode: { type: String, required: true },
-  redPepsiQuantity: { type: Number, default: 0 }, // Quantity of Red Pepsi
-  blackPepsiQuantity: { type: Number, default: 0 }, // Quantity of Black Pepsi
-  yellowPepsiQuantity: { type: Number, default: 0 }, // Quantity of Yellow Pepsi
-  totalAmount: { type: Number, required: true }, // Total amount of the order
->>>>>>> ed1c452c31e5955f55201279ac32c3d21566a77d
+  paymentStatus: { type: String, require: true },
 });
 
 const Order = mongoose.model("Order", OrderSchema);
